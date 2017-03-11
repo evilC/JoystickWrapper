5772d6f6878f8d8b1366ad141ccbda1055d2a6e6
Clive Galway <evilc@evilc.com>
2017-03-11 15:37:25 +0000

    Test script re-order

--- TestApp/Test.cs
+++ TestApp/Test.cs
@@ -22,35 +22,32 @@ namespace TestApp
             //var guid = devs[0].Guid;
             //var guid = "da2e2e00-19ea-11e6-8002-444553540000";  // Hard-code a specific stick here if you wish
 
-            // Demo - three subscriptions requested
-
-            // Subscription #1 - Axis 1 (X)
+            // Anonymous funcs to monitor callbacks
             dynamic axisHandler = new Action<int>((value) =>
             {
-                Console.WriteLine("First Axis Value: " + value);
+                Console.WriteLine("Axis Value: " + value);
             });
-
-            jw.SubscribeAxis(dev.Guid, 1, axisHandler, "LV1");
-
-            // Subscription #2 - Button 128
             dynamic buttonHandler = new Action<int>((value) =>
             {
-                Console.WriteLine("First Button Value: " + value);
+                Console.WriteLine("Button Value: " + value);
             });
-
-            jw.SubscribeButton(dev.Guid, 1, buttonHandler);
-
-            // Subscription #3 - POV 4
             dynamic povHandler = new Action<int>((value) =>
             {
-                Console.WriteLine("First POV Value: " + value);
+                Console.WriteLine("POV Value: " + value);
             });
-
-            jw.SubscribePov(dev.Guid, 1, povHandler);
 
             //jw.UnSubscribeAxis(dev.Guid, 1, "LV1");
             //jw.SubscribeAxis(dev.Guid, 1, axisHandler, "LV1");
 
+            // Demo - three subscriptions requested
+            // Subscription #1 - Axis 1 (X)
+            jw.SubscribeAxis(dev.Guid, 1, axisHandler, "LV1");
+
+            // Subscription #2 - Button 128
+            jw.SubscribeButton(dev.Guid, 1, buttonHandler);
+
+            // Subscription #3 - POV 4
+            jw.SubscribePov(dev.Guid, 1, povHandler);
         }
     }
 }
